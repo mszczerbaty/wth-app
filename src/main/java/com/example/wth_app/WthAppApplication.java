86099d1f6cd08f1f8d18e4957a8165cd43cdafe4
marcin <marcin.rafal6@gmail.com>
2025-02-25 15:46:00 +0100

    init

---
+++ src/main/java/com/example/wth_app/WthAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.wth_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WthAppApplication.class, args);
+	}
+
+}
