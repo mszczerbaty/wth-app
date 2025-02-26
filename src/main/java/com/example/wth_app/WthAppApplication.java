f671d5ab5d1d86d8139d7ce2dfaf87db0fb5e47c
marcin <marcin.rafal6@gmail.com>
2025-02-26 12:49:25 +0100

    cache weatherClient response

--- src/main/java/com/example/wth_app/WthAppApplication.java
+++ src/main/java/com/example/wth_app/WthAppApplication.java
@@ -2,8 +2,10 @@ package com.example.wth_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication(scanBasePackages = "com.example.wth_app")
+@SpringBootApplication
+@EnableCaching
 public class WthAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/wth_app/client/WeatherClient.java
+++ src/main/java/com/example/wth_app/client/WeatherClient.java
@@ -2,6 +2,7 @@ package com.example.wth_app.client;
 
 import com.example.wth_app.dto.WeatherResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherClient {
     private final RestTemplate restTemplate;
 
+    @Cacheable(value = "weather", key = "#city", unless = "#result == null")
     public WeatherResponse getWeather(String city, String apiKey) {
         String OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
         return restTemplate.getForObject(OPEN_WEATHER_URL, WeatherResponse.class, city, apiKey);
